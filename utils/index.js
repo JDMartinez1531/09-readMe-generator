d30dd27f47decde18a86360acf13a4afd55025e0
JDMartinez1531 <63159799+JDMartinez1531@users.noreply.github.com>
2020-06-25 11:57:57 -0500

    prompts for file write location and built github client

--- utils/index.js
+++ utils/index.js
@@ -1,17 +1,56 @@
 const fs = require("fs");
 const util = require("util");
+const inquirer = require("inquirer");
+var github = require("octonode");
 const userPrompts = require("./prompts")
-const generateMkdown = require("./generateMarkdown")
+const generateMkdown = require("./generateMarkdown");
+const prompts = require("./prompts");
 
-const questions = [
-
-];
-
-function writeToFile(fileName, data) {
+// prompt file write location
+let init = async function() { 
+      try {
+    const { location } = await inquirer.prompt([
+{
+    name: "location",
+    type: "list",
+    message: "Where would you like to write your README.md",
+    choices: ["Create new Github repository", "Local"],
+ }
+]);
+if (location === "Create new Github repository") {
+const { username, password } = await inquirer.prompt([{
+    name: "username",
+    message: "Github Username: ",
+    type: "input",
+  },
+  {
+    name: "password",
+    message: "Github Password: ",
+    type: "password",
+  }])
+     var client = github.client({
+        username: username,
+        password: password,
+      });
+getAuthentication(client)
+}
+}
+catch (err) {
+    console.log(err);
+  }
 }
 
-function init() {
-
+// get github authentication
+function getAuthentication(client) {
+  client.get("/user", {}, function (err, status, body, headers) {
+    if (!err) {
+      authenticated = true;
+      console.log("authenticated");
+    //   newRepoPrompts();
+    } else {
+      authenticated = false;
+      console.log("Invalid credentials");
+    }
+  });
 }
-
 init();
--- utils/prompts.js
+++ utils/prompts.js
@@ -1,10 +1,22 @@
 const inquirer = require("inquirer");
 
-const questions = []
+const questions = [
+    {
+        name: "location",
+        type: "list",
+        message: "Where would you like to write your README.md",
+        choices: [
+          "Create new Github repository",
+          "Local",
+        ],
+      },
+]
 
 
 
 
-
+function prompts() {
+    return inquirer.prompt(questions)
+}
 
 module.exports = prompts;
